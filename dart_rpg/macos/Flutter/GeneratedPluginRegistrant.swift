29c3fc79fa8d3d8361882a1f08031d8bcea9daac
Craig Smith <agent.craig@gmail.com>
2025-04-10 02:07:48 -0700

    updated packages

--- dart_rpg/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dart_rpg/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,18 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
 import file_picker
 import file_selector_macos
 import path_provider_foundation
+import quill_native_bridge_macos
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  QuillNativeBridgePlugin.register(with: registry.registrar(forPlugin: "QuillNativeBridgePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }

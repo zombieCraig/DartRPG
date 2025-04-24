82aff5613c32a22ecdd5c34128e248c80173fc09
zombieCraig <zombieCraig@users.noreply.github.com>
2025-04-24 00:57:50 +0000

    deploy: ef415024754e91196da7edce4be4709d3e9fef87

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1035039022"
+    serviceWorkerVersion: "1022544253"
   }
 });
